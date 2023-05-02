f5fcf5a7c15f150ee44ea3e847a774cb7f51d744
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-05-02 16:34:56 -0500

    Update 02/05/23
    Manejo de SSR ---> para renderizado de la pagina por parte del servidor
    
    creacion del componente product ---> con todo respecto a obtener y almacenar la informacion del producto (id, title, price, image)
    
    solicitud get a la api de plaz ---> HttpClient maneja el get para poder obtener la información

--- Dev-app/src/app/app.component.ts
+++ Dev-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgOptimizedImage} from '@angular/common';
+import { product } from './models/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Dev-app';
+  http = inject(HttpClient);
+  products: product[] = [];
+
+  changeTitle() {
+    this.title = 'Cambio de titulo';
+  }
+  ngOnInit() {
+    this.http.get<product[]>('https://api.escuelajs.co/api/v1/products').subscribe((data) => {
+      console.log(data);
+      this.products = data;
+    });
+  }
 }
